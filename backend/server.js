25677e99e1df487d2a4e0660059aa4e9247dfb3d
saad <saad.bin@saad.com>
2024-07-25 19:59:00 +0500

    mongo db data base created and connected

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./workout')
 
 
@@ -17,9 +18,19 @@ app.use((req,res,next) => {
 
 app.use('/api/workouts', workoutRoutes)
 
+//connect to db
 
-//listen for request
+mongoose.connect(process.env.MONGO_URI).then(() => {
+
+    //listen for request
 app.listen(process.env.PORT, () => {
     console.log(`listening on port ${process.env.PORT}`)
 })
+
+
+}).catch((error) => {
+    console.log(error)
+})
+
+
 
