d150eb057db245110e1f1017d60ede39512f5974
saad <saad.bin@saad.com>
2024-07-26 23:32:07 +0500

    form added

--- frontend/src/components/WorkoutForm.js
+++ frontend/src/components/WorkoutForm.js
@@ -4,11 +4,42 @@ function WorkoutForm() {
 const [title,setTitle] = useState('')
 const [reps,setReps] = useState('')
 const [load,setLoad] = useState('')
+const [error,setError] = useState('')
+
+
+const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    const workout = {title, load,reps}
+
+    const response = await fetch('/api/workouts', {
+        method: 'POST',
+        body: JSON.stringify(workout),
+        headers: {
+            'Content-Type' : 'application/json'
+        }
+        
+    })
+
+    const json = await response.json()
+
+    if(!response.ok) {
+
+        setError(json.error)
+
+    }if(response.ok){
+        setTitle('')
+        setReps('')
+        setLoad('')
+        setError(null)
+        console.log("new workout added",json)
+    }
+}
   return (
     <div className='workout-form'>
          <div className="content">
 
-      <form>
+      <form className='create' onSubmit={handleSubmit}>
         <h3>Add a Workout</h3>
         <label>Exercise Title:</label>
         <input type='text'
